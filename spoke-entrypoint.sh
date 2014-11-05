c8b40815a97645e0f017feed27b4c8c0ab9a313c
Brian Clements <brian@brianclements.net>
2014-11-05 01:10:12 -0800

    refactor spoke-entrypoint.sh

--- spoke-entrypoint.sh
+++ spoke-entrypoint.sh
@@ -45,29 +45,28 @@ apply_permissions() {
     fi
 }
 
-if [ -z $SPOKE_NAME ]; then
-    echo "Error: SPOKE_NAME variable not set in Dockerfile. Supervisor doesn't know what to start."
-    exit 1
-fi
-
-if [ "$SPOKE_DETACH_MODE" = "False" ]; then
-    if [ ! -d /config ]; then
-        echo "Error: No Hub container detected."
+run_spoke_checks() {
+    if [ -z $SPOKE_NAME ]; then
+        echo "Error: SPOKE_NAME variable not set in Dockerfile. Supervisor doesn't know what to start."
         exit 1
     fi
 
-    # Wait until the Hub container is done loading all configuration.
-    echo "Spoke \"$SPOKE_NAME\" is waiting for Hub container to load..."
-    while [ -d /run/hub.lock ]; do
-        sleep 1s
-    done
-    echo "Hub container loaded. Continuing to load Spoke \"$SPOKE_NAME\"."
-fi
+    if [ "$SPOKE_DETACH_MODE" = "False" ]; then
+        if [ ! -d /config ]; then
+            echo "Error: No Hub container detected."
+            exit 1
+        fi
 
+        # Wait until the Hub container is done loading all configuration.
+        echo "Spoke \"$SPOKE_NAME\" is waiting for Hub container to load..."
+        while [ -d /run/hub.lock ]; do
+            sleep 1s
+        done
+        echo "Hub container loaded. Continuing to load Spoke \"$SPOKE_NAME\"."
+    fi
+}
 
-if [ ! -e /tmp/first_run ]; then
-    touch /tmp/first_run
-
+do_first_run_tasks() {
     # Setup configuration in spoke-detach mode only
     if [ "$SPOKE_DETACH_MODE" = "True" ]; then
         if [ ! $(which git) ]; then
@@ -112,29 +111,41 @@ if [ ! -e /tmp/first_run ]; then
     # so we use $(here)s which uses the location of the configuration file
     # itself.
     ln -sf /config/supervisor/supervisord.conf /run/supervisor/$HOSTNAME/supervisord.conf
-fi
+}
 
-# The following programs need to be run after supervisor starts, however,
-# we need to run supervisor last because of `exec` in this script. So we run
-# them with a delay until after supervisor claims PID 1 through exec.
+start_normal() {
+    # The following programs need to be run after supervisor starts, however,
+    # we need to run supervisor last because of `exec` in this script. So we run
+    # them with a delay until after supervisor claims PID 1 through exec.
 
-# Start SSHD if GH_USER is set.
-if [ ! $GH_USER = "/__NULL__/" ]; then
+    # Start SSHD if GH_USER is set.
+    if [ ! $GH_USER = "/__NULL__/" ]; then
+        /bin/sh -c "while [ ! -e /run/supervisor/$HOSTNAME/supervisord.pid ]; do sleep 1s; done &&
+                    supervisorctl -s unix:///run/supervisor/$HOSTNAME/supervisor.sock start sshd" &
+    fi
+
+    # Start this spoke's program group
     /bin/sh -c "while [ ! -e /run/supervisor/$HOSTNAME/supervisord.pid ]; do sleep 1s; done &&
-                supervisorctl -s unix:///run/supervisor/$HOSTNAME/supervisor.sock start sshd" &
+        supervisorctl -s unix:///run/supervisor/$HOSTNAME/supervisor.sock start $APP_GROUP:*" &
+
+    # Copy errors to main log for easy debugging with `docker logs` if errors occur
+    # preventing application startup. No need to include normal output here
+    # because a real log management solution should be in place for that.
+    /bin/sh -c "while [ ! -e /log/$HOSTNAME/${SPOKE_NAME}_stderr.log ]; do sleep 1s; done &&
+                tail --follow=name -c +0 /log/$HOSTNAME/*_stderr.log |
+                tee -a /log/$HOSTNAME/supervisord.log" &
+
+    exec supervisord \
+        --configuration=/run/supervisor/$HOSTNAME/supervisord.conf \
+        --logfile=/log/$HOSTNAME/supervisord.log
+}
+
+
+run_spoke_checks
+
+if [ ! -e /tmp/first_run ]; then
+    touch /tmp/first_run
+    do_first_run_tasks
 fi
 
-# Start this spoke's program group
-/bin/sh -c "while [ ! -e /run/supervisor/$HOSTNAME/supervisord.pid ]; do sleep 1s; done &&
-    supervisorctl -s unix:///run/supervisor/$HOSTNAME/supervisor.sock start $APP_GROUP:*" &
-
-# Copy errors to main log for easy debugging with `docker logs` if errors occur
-# preventing application startup. No need to include normal output here
-# because a real log management solution should be in place for that.
-/bin/sh -c "while [ ! -e /log/$HOSTNAME/${SPOKE_NAME}_stderr.log ]; do sleep 1s; done &&
-            tail --follow=name -c +0 /log/$HOSTNAME/*_stderr.log |
-            tee -a /log/$HOSTNAME/supervisord.log" &
-
-exec supervisord \
-    --configuration=/run/supervisor/$HOSTNAME/supervisord.conf \
-    --logfile=/log/$HOSTNAME/supervisord.log
+start_normal
